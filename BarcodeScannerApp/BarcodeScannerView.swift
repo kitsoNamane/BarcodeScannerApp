e77af5525d691fe661fc9707b36b486666e02b89
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-11 22:04:49 +0200

    feat: initialize project

---
+++ BarcodeScannerApp/BarcodeScannerView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  BarcodeScannerApp
+//
+//  Created by Kitso Namane on 11/09/23.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    
+                Spacer().frame(height: 30)
+                    
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+            }
+            .padding()
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
