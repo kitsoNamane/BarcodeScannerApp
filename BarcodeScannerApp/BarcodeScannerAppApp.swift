8cfb383ccd979b9da082eef5014bdcdd02d17109
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-11 21:29:36 +0200

    Initial Commit

---
+++ BarcodeScannerApp/BarcodeScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerAppApp.swift
+//  BarcodeScannerApp
+//
+//  Created by Kitso Namane on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
