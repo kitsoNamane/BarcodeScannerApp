2bc4b946c813afbf8bb4ae2c637919fb07f953af
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-16 18:26:38 +0200

    feat: implement alerts properly

--- BarcodeScannerApp/BarcodeScannerView.swift
+++ BarcodeScannerApp/BarcodeScannerView.swift
@@ -7,32 +7,15 @@
 
 import SwiftUI
 
-struct AlertItem : Identifiable {
-    let id = UUID()
-    let title: String
-    let message: String
-    let dismissButton: Alert.Button
-}
-
-struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-        message: "Something is wrong with the camera. We are unable to capture the input.",
-        dismissButton: .default(Text("OK")))
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
-        message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
-        dismissButton: .default(Text("OK")))
-}
-
 struct BarcodeScannerView: View {
     
     @State private var scannedCode: String = ""
     @State private var alertItem: AlertItem?
-    @State private var isShowingAlert: Bool = false
     
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode)
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                     
                 Spacer().frame(height: 30)
@@ -46,7 +29,6 @@ struct BarcodeScannerView: View {
                     .foregroundStyle(scannedCode.isEmpty ? .red : .green)
                     .padding()
                 Button {
-                    isShowingAlert = true
                 } label: {
                     Text("Tap Me")
                 }
